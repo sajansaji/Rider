82d9f11a395d8cbaa359bfb7249999d8bd04443d
Sajan Saji <sajansaji2050@gmail.com>
2024-06-13 20:05:03 +0530

    login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import '../assets/css/login.css'
+
 import Goog from '../assets/ico/Goog'
 import Face from '../assets/ico/Face'
 import { Mail, Lock} from 'lucide-react';
@@ -10,39 +10,40 @@ const Login = () => {
 
         {/* google and Facebook */}
         <a href="" >
-          <section className=' p-2 mx-3 rounded-md border-2 border-[#DB4437FF] text-[#DB4437FF] my-4 text-center gap-2 flex items-center justify-center' >
+          <section className=' p-2 py-3 mx-3 inter rounded-md border-2 border-[#DB4437FF] text-[#DB4437FF] my-4 text-center gap-2 flex items-center justify-center' >
             <Goog />
             Continue with Google
           </section>
         </a>
         <a href="">
-          <section className=' p-2 mx-3 rounded-md border-2 text-center border-[#4267B2FF] text-[#4267B2FF] my-4 gap-2 flex items-center justify-center'>
+          <section className=' p-2 py-3 mx-3 inter rounded-md border-2 text-center border-[#4267B2FF] text-[#4267B2FF] my-4 gap-2 flex items-center justify-center'>
             <Face /> Continue with Facebook
           </section>
         </a>
-        <p className='text-center'>
+        <p className='text-center epilogue'>
 
           OR LOG IN WITH
         </p>
 
-        <div className=''>
+        <div className='inter'>
           Email Phone number
         </div>
        
 
-        <div className='mx-4'>
+        <div className='mx-4 inter'>
           <div className=' my-2'>
           
-            <label htmlFor="">Email</label><br /> <Mail /> 
+            <label htmlFor=""><b>Email</b></label><br /> <Mail /> 
             <input type="text" name="name" id="name" placeholder='      Enter email' className='bg-[#f3f4f6] py-3 w-[276px] rounded-md ' />
           </div>
-          <div className=' my-2'>
-            <label htmlFor="">Password</label> <br /> <Lock />
-            <input type="password" name="pass" id="pass" placeholder='       Enter password' className='bg-[#f3f4f6] h-[52px] w-[276px] rounded-md ' />
-            <section className='flex'><p className='text-[#00BDD6FF] '>Forgot Password? </p>
-            <input type="checkbox" name="rem" id="rem" />  Remember me</section>
+          <div className=' my-2 '>
+            <label htmlFor=""><b>Password</b></label> <br /> <Lock />
+            <input type="password" name="pass" id="pass" placeholder='       Enter password' className='bg-[#f3f4f6]  w-[276px] rounded-md  py-3 ' />
+            <section className='flex mt-2'><p className='text-[#00BDD6FF] justify-between '>Forgot Password?  </p>
+            <input type="checkbox" name="rem" id="rem" value="rem"  />  Remember me</section>
 
           </div>
+          <section className='justify-center text-center bg-[#00BDD6FF] rounded-md py-3 mt-4 text-white mb-3 inter' >Continue</section>
         </div>
 
 
