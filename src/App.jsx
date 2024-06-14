cb704e50ce8b73389870c5785832c00ca7feedd3
Sajan Saji <sajansaji2050@gmail.com>
2024-06-14 22:21:27 +0530

    Nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,53 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
+// import { NeatConfig, NeatGradient } from "@firecms/neat";
+import Header from './components/Header';
+// export const config: NeatConfig = {
+//   "colors": [
+//       {
+//           "color": "#DD95DE",
+//           "enabled": true
+//       },
+//       {
+//           "color": "#ffc8dd",
+//           "enabled": true
+//       },
+//       {
+//           "color": "#ffafcc",
+//           "enabled": true
+//       },
+//       {
+//           "color": "#5EAEF5",
+//           "enabled": true
+//       },
+//       {
+//           "color": "#2165E4",
+//           "enabled": true
+//       }
+//   ],
+//   "speed": 2,
+//   "horizontalPressure": 3,
+//   "verticalPressure": 3,
+//   "waveFrequencyX": 2,
+//   "waveFrequencyY": 4,
+//   "waveAmplitude": 5,
+//   "shadows": 0,
+//   "highlights": 2,
+//   "colorBrightness": 1,
+//   "colorSaturation": 3,
+//   "wireframe": false,
+//   "colorBlending": 5,
+//   "backgroundColor": "#003FFF",
+//   "backgroundAlpha": 1,
+//   "resolution": 1
+// }
 
 function App() {
 
   return (
     <>
+      <Header />
       <div>
-        
       </div>
     </>
   )
---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+// import React from 'react'
+import { LogIn } from 'lucide-react';
+
+const Header = () => {
+  return (
+    <>
+
+    <div className='w-full h-16 shadow-md sticky top-0 inter text-center flex gap-3 justify-center items-center content-center'>
+        {/* space for logo */}
+        <ol className="  " >
+          <li className='hover:text-blue-600'>Explore</li>
+          {/* <li></li>
+          <li></li> */}
+          
+        </ol>
+        <a href="/auth" className=' bg-[#f6f6f7] hover:bg-[#c8c8c9] shadow-md border-1 border-[#000] rounded-md p-1'><LogIn/></a> 
+    </div>
+    </>
+  )
+}
+
+export default Header
