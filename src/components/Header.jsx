eedc8980bdcbbaf88a36eb435277cfe789c06ab7
Sajan Saji <sajansaji2050@gmail.com>
2024-06-14 22:24:07 +0530

    ;

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,11 +7,9 @@ const Header = () => {
 
     <div className='w-full h-16 shadow-md sticky top-0 inter text-center flex gap-3 justify-center items-center content-center'>
         {/* space for logo */}
-        <ol className="  " >
+        <ol className=" flex gap-3 " >
           <li className='hover:text-blue-600'>Explore</li>
-          {/* <li></li>
-          <li></li> */}
-          
+          <li className='hover:text-blue-600'>Plan</li>
         </ol>
         <a href="/auth" className=' bg-[#f6f6f7] hover:bg-[#c8c8c9] shadow-md border-1 border-[#000] rounded-md p-1'><LogIn/></a> 
     </div>
