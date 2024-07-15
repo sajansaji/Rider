2700384fd2c85bd71dbd8b12b4c7b87714486686
Sajan Saji <sajansaji2050@gmail.com>
2024-07-15 23:26:55 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -36,11 +36,11 @@ const Login = () => {
           <div className=' my-2'>
           
             <label htmlFor=""><b>Email</b></label><br /> <Mail /> 
-            <input type="text" name="name" id="name" placeholder=' &#xf0e0; Enter your email' className='bg-[#f3f4f6] py-3 w-[276px] lg:w-[350px] rounded-md ' />
+            <input type="text" name="name" id="name" placeholder=' &#xf0e0; Enter your email' className='bg-[#f3f4f6] py-3 p-4 w-[276px] lg:w-[350px] rounded-md ' />
           </div>
           <div className=' my-2 '>
             <label htmlFor=""><b>Password</b></label> <br /> <Lock />
-            <input type="password" name="pass" id="pass" placeholder='  &#xf023; Enter password' className='bg-[#f3f4f6]  w-[276px] lg:w-[350px] rounded-md  py-3 ' />
+            <input type="password" name="pass" id="pass" placeholder='  &#xf023; Enter password' className='bg-[#f3f4f6] p-4 w-[276px] lg:w-[350px] rounded-md  py-3 ' />
             <section className='flex mt-2'><p className='text-[#00BDD6FF] justify-between '>Forgot Password?  </p>
             <input type="checkbox" name="rem" id="rem" value="rem"  />  Remember me</section>
 
