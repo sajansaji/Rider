cab7a5d183ef0563dc4ac5ae9911d82d9285556f
Sajan Saji <sajansaji2050@gmail.com>
2024-07-16 23:06:44 +0530

    style

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Header from './components/Header';
 import './assets/css/app.css';
 
+
 const App = () => {
 
   return (
     <>
       <Header />
-      <div className=" flex justify-end px-36 space-x-4 pt-64  ">
-        <input type='text' placeholder='Starting point' className=' p-2 rounded-md bg-[#00ed64] border-2 border-[#1e508c]'></input>
+      <div className=" flex justify-end px-36 space-x-4 pt-64 z-10 ">
+        <input type='text' placeholder='Starting point' className=' p-2 rounded-md bg-[#6ea284] border-2 border-[#1e508c]'></input>
         <input type='text' placeholder='Destination' className=' p-2 rounded-md bg-[#00ed64] border-2 border-[#1e508c]'></input>
         <button className=' py-2 px-7 rounded-md hover:bg-[#00ed00] bg-[#00ed64] border-[#1e508c] border-2'>Search</button>
       </div>
