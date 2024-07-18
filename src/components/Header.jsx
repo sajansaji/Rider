ab9c4d30d36a41f73df25008e0b52c2f61a45358
Sajan Saji <sajansaji2050@gmail.com>
2024-07-18 22:47:27 +0530

    placeholder styled

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <>
 
-      <div className='w-full h-16 shadow-md sticky inter text-center flex gap-3 justify-end pr-14 items-center content-center bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 backdrop-blur-sm'>
+      <div className='w-full h-16 shadow-md sticky fredoka text-center flex gap-3 justify-end pr-14 items-center content-center bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 backdrop-blur-sm'>
         {/* space for logo */}
         <ol className=" flex gap-3 text-white" >
           <li className=' hover:bg-[#dbe9e1] hover:text-slate-900 hover:shadow-md border-1 px-2 rounded-full p-1'>Explore</li>
